b3e1e3f1894d4b9fe2cf441ffadbc8e1ca5c7041
shbone <598924626@qq.com>
2023-11-02 20:31:15 +0800

    init project

---
+++ yantu_python_utils/test/operate_json_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： sunhb
+# datetime： 2023/11/2 下午4:48 
+# ide： PyCharm
+# filename: operate_json_test.py
+def test_operate_json():
+    from yantu_python_utils.operate_json import write_json
+    write_json(["tes"],filepath="../test/test.json")
+    pass
+
